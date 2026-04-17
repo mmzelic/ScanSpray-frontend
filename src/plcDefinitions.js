97c98e64e57562969444e59061e7cd265cac3e0f
Murilo Zelic <murilo.zelic@graymatter-robotics.com>
2026-04-17 13:52:15 -0700

    css change

--- src/plcDefinitions.js
+++ src/plcDefinitions.js
@@ -46,5 +46,6 @@ export const reads = [
   { name: "Recipe Echo", addr: 230, bit: null },
   { name: "Active Recipe", addr: 231, bit: null },
   { name: "Robot At Home", addr: 250, bit: 0 },
-  { name: "Robot Cycle Complete", addr: 250, bit: 1 }
+  { name: "Robot Cycle Complete", addr: 250, bit: 1 },
+  { name: "PLC Version", addr: 299, bit: null }
 ];
